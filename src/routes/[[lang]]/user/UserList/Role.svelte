c96972d29179d57357969b00ecef214f4af73fee
jaksakecojevic <66748820+jaksakecojevic@users.noreply.github.com>
2023-03-16 06:10:27 +0100

    Rename colList, dark mode icon fix, added role to user table
    Collections_latest renamed to CollectionsList
    Dark/light mode icon was not showing the proper icon now its fixed
    Added colored roles in the user table

---
+++ src/routes/[[lang]]/user/UserList/Role.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	export let value: string;
+	let classes = `${
+		value === 'Admin' ? 'gradient-tertiary text-white' : 'bg-amber-400 text-black'
+	} p-2 rounded`;
+</script>
+
+<div class={classes}>
+	{value}
+</div>
