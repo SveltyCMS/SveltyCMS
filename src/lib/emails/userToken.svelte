0dae2f3b6d62d47d6a20152ee2a93d99741765e0
Rar9 <RKroells@web.de>
2023-03-24 18:44:59 +0100

    usertoken update

--- src/lib/emails/userToken.svelte
+++ src/lib/emails/userToken.svelte
@@ -1,9 +1,16 @@
 <script>
 	import { PUBLIC_SITENAME } from '$env/static/public';
 	import SimpleCmsLogo from '$src/components/icons/SimpleCMS_Logo.svelte';
+	import { HOST } from '$env/static/private';
 
 	// svelte-email
 	import { Button, Container, Head, Hr, Html, Img, Preview, Section, Text } from 'svelte-email';
+
+	export let email = 'test@test.de';
+	export let role = 'admin';
+	export let token = 'token';
+	export let expires_in = '2hrs';
+	export let tokenLink = { HOST };
 
 	const fontFamily =
 		'-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen-Sans,Ubuntu,Cantarell,"Helvetica Neue",sans-serif';
@@ -27,9 +34,9 @@
 		lineHeight: '26px'
 	};
 
-	const btnContainer = {
-		textAlign: 'center' as const
-	};
+	// const btnContainer = {
+	// 	textAlign: 'center' as const
+	// };
 
 	const button = {
 		fontFamily,
@@ -38,7 +45,7 @@
 		color: '#fff',
 		fontSize: '16px',
 		textDecoration: 'none',
-		textAlign: 'center' as const,
+		// textAlign: 'center' as const,
 		display: 'block'
 	};
 
@@ -56,18 +63,23 @@
 
 <Html lang="en">
 	<Head>
-		<title>User Registration token to access {PUBLIC_SITENAME}</title>
-		<meta name="description" content="User Registration token to access {PUBLIC_SITENAME}" />
+		<title>User Registration token for {PUBLIC_SITENAME}</title>
+		<meta name="description" content="User Registration token for {PUBLIC_SITENAME}" />
 	</Head>
-	<Preview preview="User Registration token to access {PUBLIC_SITENAME}" />
+	<Preview preview="User Registration token for {PUBLIC_SITENAME}" />
 	<Section style={main}>
 		<Container style={container}>
 			<SimpleCmsLogo fill="red" className="h-8" />
 			<Text style={paragraph}
-				>You have recieved an access Token valid for XX to create a new user for {PUBLIC_SITENAME}</Text
+				>You have recieved an Access Token to create a new user for {PUBLIC_SITENAME}</Text
 			>
-			<Text style={paragraph}>Please press the button to setup your user</Text>
-			<Section style={btnContainer}>
+			<Text style={paragraph}>Email: {email}</Text>
+			<Text style={paragraph}>Access Token: {token}</Text>
+			<Text style={paragraph}>Role: {role}</Text>
+			<Text style={paragraph}>valid for: {expires_in}</Text>
+
+			<Text style={paragraph}>Please press the button to setup your user with this email</Text>
+			<Section>
 				<Button pX={12} pY={12} style={button} href={tokenLink}>Create User</Button>
 			</Section>
 			<Hr style={hr} />
