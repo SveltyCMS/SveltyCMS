bb788dd0299b79d5df98fe66a952ac6cf3dabf65
Rar9 <RKroells@web.de>
2023-03-28 09:38:20 +0200

    I18n Spanish

--- src/lib/i18n/i18n-util.async.ts
+++ src/lib/i18n/i18n-util.async.ts
@@ -8,6 +8,7 @@ import { loadedFormatters, loadedLocales, locales } from './i18n-util'
 const localeTranslationLoaders = {
 	de: () => import('./de'),
 	en: () => import('./en'),
+	es: () => import('./es'),
 }
 
 const updateDictionary = (locale: Locales, dictionary: Partial<Translations>): Translations =>
--- src/lib/i18n/i18n-util.sync.ts
+++ src/lib/i18n/i18n-util.sync.ts
@@ -7,10 +7,12 @@ import { loadedFormatters, loadedLocales, locales } from './i18n-util'
 
 import de from './de'
 import en from './en'
+import es from './es'
 
 const localeTranslations = {
 	de,
 	en,
+	es,
 }
 
 export const loadLocale = (locale: Locales): void => {
