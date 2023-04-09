6147b2f513bd3d95bad893cc5ec5ca4c4b09469a
Rar9 <RKroells@web.de>
2023-04-09 13:26:03 +0200

    feat: Add Semantic Versioning

---
+++ release.config.js
@@ -0,0 +1,18 @@
+const config = {
+	branches: ['main'],
+	plugins: [
+		'@semantic-release/commit-analyzer',
+		'@semantic-release/release-notes-generator',
+		'@semantic-release/npm',
+		[
+			'@semantic-release/git',
+			{
+				assets: ['package.json'],
+				message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+			}
+		],
+		'@semantic-release/github'
+	]
+};
+
+module.exports = config;
