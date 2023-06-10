01861d31e543452b42fa688a7fb179a930c3804a
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2023-06-10 16:34:53 +0200

    Update inlang.config.js
    Using the new plugin API

--- inlang.config.js
+++ inlang.config.js
@@ -2,18 +2,12 @@
  * @type {import("@inlang/core/config").DefineConfig}
  */
 export async function defineConfig(env) {
-	// initialize the plugin
-	const plugin = await env.$import(
-		'https://cdn.jsdelivr.net/gh/ivanhofer/inlang-plugin-typesafe-i18n/dist/index.js'
-	);
+  // initialize the plugin
+  const { default: typesafeI18nPlugin } = await env.$import(
+    "https://cdn.jsdelivr.net/gh/ivanhofer/inlang-plugin-typesafe-i18n@2/dist/index.js"
+  )
 
-	// get the locale information from `typesafe-i18n`
-	const { base, locales } = await plugin.getLocaleInformation(env.$fs);
-
-	return {
-		referenceLanguage: base,
-		languages: locales,
-		readResources: (args) => plugin.readResources({ ...args, ...env }),
-		writeResources: (args) => plugin.writeResources({ ...args, ...env })
-	};
+  return {
+    plugins: [typesafeI18nPlugin()],
+  }
 }
