42b095c882b78db07bf024c6d22601fe54166966
Mohammad Alwarawreh <96836788+WarMhmd@users.noreply.github.com>
2023-10-05 16:18:47 +0300

    Fix merge issues (#77)
    * Fix email language and Upgrade token generation
    
    * Fix Admin area, upgrade token system, fix email and more.
    
    * Fixed GraphQL & collections & other
    
    * Fix email
    
    * Fixed AdminArea tokens
    
    * Changed some comments
    
    * Upgrade login experience & Admin Area
    
    * Fix merge issues

--- src/routes/api/user/editToken/+server.ts
+++ src/routes/api/user/editToken/+server.ts
@@ -11,7 +11,7 @@ export const POST: RequestHandler = async ({ request }) => {
 	if (!user) return new Response(JSON.stringify('User not found'), { status: 404 });
 	if (adminLength == 1 && data.role != 'admin' && (user as any).role == 'admin')
 		return new Response(JSON.stringify('Cannot delete all admins'), { status: 400 });
-	console.log(data);
+	adminLength -= (user as any).role == 'admin' && data.role != 'admin' ? 1 : 0;
 
 	auth.updateUserAttributes(data.userId, {
 		role: data.role
