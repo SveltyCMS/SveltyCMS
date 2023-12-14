bf08b9112b3987ccd483eaffb846193b9fd58846
Wasim Akhtar Khan <getwasim@users.noreply.github.com>
2023-12-14 21:26:59 +0530

    Forgot Password (#95)
    * chore:testing access
    
    * chore:playwright issue
    
    * chore:run local server
    
    * chore:install pnpm
    
    * chore:instal pnpm and run test
    
    * chore: fix test cases
    
    * chore:fix other two test
    
    * chore:fix signout test
    
    * feat: add comments
    
    * chore: forgot password test
    
    * chore: added new user test cases
    
    * chore:playwright.yaml fix
    
    * chore: user login issue
    
    * chore: forgot password issue
    
    * chore: modify playwright.yaml file
    
    * chore: modify playwright.yaml file
    
    * chore: modify playwright configuration
    
    * chore: modify playwright env configuration
    
    * chore: test with one test case only
    
    * chore: testing with all test
    
    * chore: testing with user page test
    
    * chore: playwright with more runners
    
    * chore: fix user page
    
    * chore: fix url
    
    * chore:remove workers
    
    ---------
    
    Co-authored-by: Rar9 <RKroells@web.de>

---
+++ tests/user.spec.ts
@@ -0,0 +1,102 @@
+import { test } from '@playwright/test';
+
+// test('Login User', async ({ page }) => {
+// 	await page.goto('http://localhost:4173/login');
+// 	await page.locator('form').filter({ hasText: 'Email Address * Password * Sign In' }).locator('#email-address').fill('test@test.de');
+// 	await page.locator('form').filter({ hasText: 'Email Address * Password * Sign In' }).locator('#password').fill('Test123!');
+// 	await page.getByRole('button', { name: 'Sign In' }).click();
+// 	await page.goto('http://localhost:4173/user');
+// });
+
+test('Edit Avatar', async ({ page }) => {
+	await page.goto('http://localhost:4173/login');
+	await page.locator('form').filter({ hasText: 'Email Address * Password * Sign In' }).locator('#email-address').fill('test@test.de');
+	await page.locator('form').filter({ hasText: 'Email Address * Password * Sign In' }).locator('#password').fill('Test123!');
+	await page.getByRole('button', { name: 'Sign In' }).click();
+	await page.locator('#page-content').filter({ hasText: 'Collections' }).nth(0).waitFor({ state: 'visible' });
+	await page.goto('http://localhost:4173/user');
+	await page.locator('button').filter({ hasText: 'Edit Avatar' }).nth(0).waitFor({ state: 'visible' });
+	await page.getByRole('button').filter({ hasText: 'Edit Avatar' }).click();
+	await page.locator('input[type="file"]').waitFor({ state: 'visible', timeout: 60000 });
+	const fileInput = await page.locator('input[type="file"]');
+	await fileInput.setInputFiles('/download.png');
+	page.on('filechooser', async (fileChooser) => {
+		await fileChooser.setFiles('/download.png');
+	});
+	await page.getByRole('button').filter({ hasText: 'Save' }).click();
+});
+
+// test('Delete Avatar', async ({ page }) => {
+// 	await page.goto('http://localhost:4173/login');
+// 	await page.locator('form').filter({ hasText: 'Email Address * Password * Sign In' }).locator('#email-address').fill('test@test.de');
+// 	await page.locator('form').filter({ hasText: 'Email Address * Password * Sign In' }).locator('#password').fill('Test123!');
+// 	await page.getByRole('button', { name: 'Sign In' }).click();
+// 	await page.waitForSelector('div:has-text("Names")');
+// 	await page.goto('http://localhost:4173/user');
+// 	await page.getByRole('button').filter({ hasText: 'Edit Avatar' }).click();
+// 	await page.locator('span').filter({ hasText: 'Delete' }).click();
+// });
+
+// test('Edit User Details', async ({ page }) => {
+// 	await page.goto('http://localhost:4173/login');
+// 	await page.locator('form').filter({ hasText: 'Email Address * Password * Sign In' }).locator('#email-address').fill('test@test.de');
+// 	await page.locator('form').filter({ hasText: 'Email Address * Password * Sign In' }).locator('#password').fill('Test123!');
+// 	await page.getByRole('button', { name: 'Sign In' }).click();
+// 	await page.waitForSelector('div:has-text("Names")');
+// 	await page.goto('http://localhost:4173/user');
+// 	await page.locator('button').filter({ hasText: 'Edit User Settings:' }).click();
+// 	await page.locator('#username').fill('Test User');
+// 	await page.locator('#password').fill('Test123!');
+// 	await page.locator('#confirm_password').fill('Test123!');
+// 	await page.getByRole('button').filter({ hasText: 'Save' }).click();
+// });
+
+// test('Registration Token', async ({ page }) => {
+// 	await page.goto('http://localhost:4173/login');
+// 	await page.locator('form').filter({ hasText: 'Email Address * Password * Sign In' }).locator('#email-address').fill('test@test.de');
+// 	await page.locator('form').filter({ hasText: 'Email Address * Password * Sign In' }).locator('#password').fill('Test123!');
+// 	await page.getByRole('button', { name: 'Sign In' }).click();
+// 	await page.waitForSelector('div:has-text("Names")');
+// 	await page.goto('http://localhost:4173/user');
+// 	await page.locator('span').filter({ hasText: 'Email User Registration token' }).click();
+// 	await page.locator('#email-address').fill('test@test.ge');
+// 	const roleElement = await page.$('span.capitalize:has-text("user")');
+// 	if (roleElement) {
+// 		await roleElement.click();
+// 	}
+// 	const timeEkement = await page.$('span.capitalize:has-text("12 hrs")');
+// 	if (timeEkement) {
+// 		await timeEkement.click();
+// 	}
+// 	await page.getByRole('button').filter({ hasText: 'Send' }).click();
+// });
+
+// test('Show or Hide User Token', async ({ page }) => {
+// 	await page.goto('http://localhost:4173/login');
+// 	await page.locator('form').filter({ hasText: 'Email Address * Password * Sign In' }).locator('#email-address').fill('test@test.de');
+// 	await page.locator('form').filter({ hasText: 'Email Address * Password * Sign In' }).locator('#password').fill('Test123!');
+// 	await page.getByRole('button', { name: 'Sign In' }).click();
+// 	await page.waitForSelector('div:has-text("Names")');
+// 	await page.goto('http://localhost:4173/user');
+// 	await page.locator('span').filter({ hasText: 'Show User Token' }).click();
+// 	await page.locator('h2').filter({ hasText: 'Token List:' });
+// 	await page.locator('span').filter({ hasText: 'Hide User Token' }).click();
+// 	const listElement = await page.locator('h2').filter({ hasText: 'Token List:' });
+// 	const isVisible = await listElement.isVisible();
+// 	expect(isVisible).toEqual(false);
+// });
+
+// test('Show or Hide User List', async ({ page }) => {
+// 	await page.goto('http://localhost:4173/login');
+// 	await page.locator('form').filter({ hasText: 'Email Address * Password * Sign In' }).locator('#email-address').fill('test@test.de');
+// 	await page.locator('form').filter({ hasText: 'Email Address * Password * Sign In' }).locator('#password').fill('Test123!');
+// 	await page.getByRole('button', { name: 'Sign In' }).click();
+// 	await page.waitForSelector('div:has-text("Names")');
+// 	await page.goto('http://localhost:4173/user');
+// 	await page.locator('span').filter({ hasText: 'Show User List' }).click();
+// 	await page.locator('h2').filter({ hasText: 'User List:' });
+// 	await page.locator('span').filter({ hasText: 'Hide User List' }).click();
+// 	const listElement = await page.locator('h2').filter({ hasText: 'User List:' });
+// 	const isVisible = await listElement.isVisible();
+// 	expect(isVisible).toEqual(false);
+// });
