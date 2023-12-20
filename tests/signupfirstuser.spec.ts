a89b6de5c0d11068e905ca4316b6aa1fd00cccba
Wasim Akhtar Khan <getwasim@users.noreply.github.com>
2023-12-21 02:39:48 +0530

    Test cases (#98)
    * chore: fix playwright issue
    
    * chore: fix failing test
    
    * chore: fix signin issue
    
    * chore:increase worker
    
    * chore: fix edit avatar test case
    
    * chore: fix test cases
    
    ---------
    
    Co-authored-by: Rar9 <RKroells@web.de>

--- tests/signupfirstuser.spec.ts
+++ tests/signupfirstuser.spec.ts
@@ -2,23 +2,22 @@ import { test } from '@playwright/test';
 
 test('Test loading', async ({ page }) => {
 	await page.goto('http://localhost:4173/', { waitUntil: 'domcontentloaded' });
-
-	// await page.screenshot({ path: 'error-screenshot.png' });
 	console.log('Current URL:', page.url());
+  
 	await page.goto('http://localhost:4173/login', { waitUntil: 'domcontentloaded' });
-	// await expect(page).toHaveTitle(/SimpleCMS/)
 
 	// Expect this page
 	await page.locator('p').filter({ hasText: 'Sign Up' });
 	await page.locator('p').filter({ hasText: 'Sign In' });
 	await page.getByText('SimpleCMS').nth(2);
 
-	//await expect(page).toHaveTitle(/SimpleCMS/);
 });
 
 // Test language selection and button text
 test('check language selection and button text', async ({ page }) => {
 	await page.goto('http://localhost:4173/login');
+  
+	console.log('check language selection and button text');
 	const inputSelector = 'input[type="text"][list="locales"]';
 	await page.fill(inputSelector, 'de');
 	await page.waitForSelector(inputSelector, { state: 'visible', timeout: 60000 });
@@ -31,34 +30,22 @@ test('check language selection and button text', async ({ page }) => {
 // Test Signup First User
 test('SignUp First User', async ({ page }) => {
 	await page.goto('http://localhost:4173/login');
+	console.log('Test SignUp First User');
+  
 	await page.locator('p').filter({ hasText: 'Sign Up' }).click();
 	await page.locator('div').filter({ hasText: 'Sign Up' });
 	await page.locator('span').filter({ hasText: ': User' });
-	// await page.getByText('Sign Up : Admin').click();
+
 	// Test Username
-	// await page.locator('#username').click();
 	await page.locator('#username').fill('T');
 	await page.locator('#username').press('Tab');
 	await page.locator('span').filter({ hasText: 'Name must be at least 2 characters' });
-	// await page.getByText('String must contain at least 2 character(s)').click();
-	// await page.locator('#username').click();
+
 	await page.locator('#username').fill('Test');
 
 	//Test Email
-	// await page
-	// 	.locator('form')
-	// 	.filter({ hasText: 'Username * Email Address * Password * Confirm Password * Sign Up OAuth' })
-	// 	.locator('#email-address')
-	// 	.click();
 	await page.locator('form').filter({ hasText: 'Username * Email Address * Password * Confirm Password *' }).locator('#email-address').fill('tes');
-	// 	await page.locator('section').filter({ hasText: 'SimpleCMS Sign Up : Admin * Required Username * Email Address * Password * Confi' }).click();
-	// await page.getByText('Email must be a valid email');
 	await page.locator('span').filter({ hasText: 'Email must be a valid email' });
-	// 	await page
-	// 		.locator('form')
-	// 		.filter({ hasText: 'Username * Email Address * Invalid email Password * Confirm Password * Sign Up O' })
-	// 		.locator('#email-address')
-	// 		.click();
 	await page
 		.locator('form')
 		.filter({ hasText: 'Username * Email Address * Password * Confirm Password *' })
@@ -66,37 +53,18 @@ test('SignUp First User', async ({ page }) => {
 		.fill('test@test2.de');
 	await page.locator('form').filter({ hasText: 'Username * Email Address * Password * Confirm Password *' }).locator('#password').fill('Test123');
 	await page.locator('form').filter({ hasText: 'Username * Email Address * Password * Confirm Password *' }).locator('#password').press('Tab');
-	// await page.getByText('Password must be a minimum of 8 characters & contain at least one letter, one number, and one special character,String must contain at least 8 character(s)');
 	await page.locator('span').filter({
 		hasText:
 			'Password must be a minimum of 8 characters & contain at least one letter, one number, and one special character,String must contain at least 8 character(s)'
 	});
-	// 	await page
-	// 		.locator('form')
-	// 		.filter({ hasText: 'Username * Email Address * Password * Invalid,String must contain at least 8 cha' })
-	// 		.getByRole('button')
-	// 		.first()
-	// 		.click();
-	// 	await page
-	// 		.locator('form')
-	// 		.filter({ hasText: 'Username * Email Address * Password * Invalid,String must contain at least 8 cha' })
-	// 		.locator('#password')
-	// 		.click();
+
 	await page
 		.locator('form')
 		.filter({ hasText: 'Username * Email Address * Password * Password must be a minimum of 8 characters' })
 		.locator('#password')
 		.fill('Test123!');
-	// 	await page.locator('#confirm-password').click();
 	await page.locator('#confirm-password').fill('Test1234!');
 	await page.getByText('Password & Confirm password must match');
-	// 	await page
-	// 		.locator('form')
-	// 		.filter({ hasText: 'Username * Email Address * Password * Confirm Password * Sign Up OAuth' })
-	// 		.locator('#password')
-	// 		.click();
-	// 	//missing Error message for Confirm Password
-	// 	await page.locator('#confirm-password').click();
 	await page.locator('#confirm-password').fill('Test123!');
 	await page.getByRole('button', { name: 'Sign Up' }).click();
 	// 	//Test not login to Dashboard
@@ -106,6 +74,8 @@ test('SignUp First User', async ({ page }) => {
 // Signout user after login
 test('SignOut', async ({ page }) => {
 	await page.goto('http://localhost:4173/login');
+	console.log('Signing out test');
+  
 	await page.locator('p').filter({ hasText: 'Sign In' }).click();
 	await page
 		.locator('form')
@@ -131,18 +101,12 @@ test('SignOut', async ({ page }) => {
 
 test('Login First User', async ({ page }) => {
 	await page.goto('http://localhost:4173/login');
+	console.log('Login First User');
+  
 	await page.locator('p').filter({ hasText: 'Sign In' }).click();
-	// await page.locator('form').locator('#email-address').nth(0).waitFor();
-	// await page.locator('form').locator('#email-address').nth(0).fill('test', { timeout: 60000 });
 	await page.locator('form').filter({ hasText: 'Email Address * Password * Sign In' }).locator('#email-address').fill('test', { timeout: 60000 });
-	// await page.locator('form').filter({ hasText: 'Email Address * Password * Sign In OAuth Forgotten Password' }).locator('#password').click();
-	// await page.getByText('Email must be a valid email');
 	await page.locator('span').filter({ hasText: 'Email must be a valid email' });
-	// await page.locator('form').locator('#email-address').nth(0).fill('test@test2.de');
-	// await page.locator('form').locator('#password').nth(0).fill('Test123!');
-	// await page.locator('form').filter({ hasText: 'Email Address * Invalid email Password * Sign In OAuth Forgotten Password' }).locator('#email-address').click();
 	await page.locator('form').filter({ hasText: 'Email Address * Password * Sign In' }).locator('#email-address').fill('test@test2.de');
-	// await page.locator('form').filter({ hasText: 'Email Address * Password * Sign In OAuth Forgotten Password' }).locator('#password').click();
 	await page.locator('form').filter({ hasText: 'Email Address * Password * Sign In' }).locator('#password').fill('Test123!');
 	await page.getByRole('button', { name: 'Sign In' }).click();
 
@@ -151,6 +115,8 @@ test('Login First User', async ({ page }) => {
 
 test('Forgot Password', async ({ page }) => {
 	await page.goto('http://localhost:4173/login');
+	console.log('Forgot Password');
+  
 	await page.locator('p').filter({ hasText: 'Sign In' }).click();
 	const forgottenPasswordButton = await page.waitForSelector('button:has-text("Forgotten Password")');
 	await forgottenPasswordButton.click();
--- tests/user.spec.ts
+++ tests/user.spec.ts
@@ -1,26 +1,30 @@
 import { test, expect } from '@playwright/test';
 
-// test for all /user
 
 test('Login User', async ({ page }) => {
 	await page.goto('http://localhost:4173/login');
+	console.log('Login User test');
+
+// test for all /user
 	await page.locator('p').filter({ hasText: 'Sign In' }).click();
 	await page.locator('form').filter({ hasText: 'Email Address * Password * Sign In' }).locator('#email-address').fill('test@test.de');
 	await page.locator('form').filter({ hasText: 'Email Address * Password * Sign In' }).locator('#password').fill('Test123!');
 	await page.getByRole('button', { name: 'Sign In' }).click();
+	page.waitForTimeout(3000);
+
 });
 
 test('Edit Avatar', async ({ page }) => {
 	await page.goto('http://localhost:4173/login');
+	console.log('Edit Avatar test');
+
 	await page.locator('p').filter({ hasText: 'Sign In' }).click();
 	await page.locator('form').filter({ hasText: 'Email Address * Password * Sign In' }).locator('#email-address').fill('test@test.de');
 	await page.locator('form').filter({ hasText: 'Email Address * Password * Sign In' }).locator('#password').fill('Test123!');
 	await page.getByRole('button', { name: 'Sign In' }).click();
-	// await page.waitForSelector('#page-content');
-	await page.waitForTimeout(5000);
-	// await page.locator('span').filter({ hasText: 'Create' }).waitFor({ state: 'visible' });
+	await page.waitForTimeout(10000);
+	await page.goto('http://localhost:4173/en/Names');
 	await page.goto('http://localhost:4173/user');
-	await page.waitForTimeout(5000);
 	await page.locator('h1').filter({ hasText: 'User Profile' }).waitFor({ state: 'visible', timeout: 60000 });
 	await page.locator('//button[text()="Edit Avatar"]').click();
 	await page.locator('input[type="file"]').waitFor({ state: 'visible', timeout: 60000 });
@@ -30,41 +34,50 @@ test('Edit Avatar', async ({ page }) => {
 		await fileChooser.setFiles('/tests/testthumb.png');
 	});
 	await page.getByRole('button').filter({ hasText: 'Save' }).click();
+	page.waitForTimeout(3000);
 });
 
 test('Delete Avatar', async ({ page }) => {
 	await page.goto('http://localhost:4173/login');
+	console.log('Delete Avatar test');
+
 	await page.locator('p').filter({ hasText: 'Sign In' }).click();
 	await page.locator('form').filter({ hasText: 'Email Address * Password * Sign In' }).locator('#email-address').fill('test@test.de');
 	await page.locator('form').filter({ hasText: 'Email Address * Password * Sign In' }).locator('#password').fill('Test123!');
 	await page.getByRole('button', { name: 'Sign In' }).click();
 	await page.waitForTimeout(10000);
-	// await page.locator('span').filter({ hasText: 'Create' }).waitFor({ state: 'visible' });
 	await page.goto('http://localhost:4173/user');
-	await page.waitForTimeout(10000);
 	await page.locator('button').filter({ hasText: 'Edit Avatar' }).click();
 	await page.locator('button.variant-filled-error').click();
+	page.waitForTimeout(3000);
 });
 
 test('Edit User Details', async ({ page }) => {
 	await page.goto('http://localhost:4173/login');
+	console.log('Edit User Details test');
+
 	await page.locator('p').filter({ hasText: 'Sign In' }).click();
 	await page.locator('form').filter({ hasText: 'Email Address * Password * Sign In' }).locator('#email-address').fill('test@test.de');
 	await page.locator('form').filter({ hasText: 'Email Address * Password * Sign In' }).locator('#password').fill('Test123!');
 	await page.getByRole('button', { name: 'Sign In' }).click();
 	await page.waitForTimeout(10000);
-	await page.waitForSelector('div:has-text("Names")');
+;
 	await page.goto('http://localhost:4173/user');
-	await page.waitForTimeout(10000);
+
 	await page.locator('button').filter({ hasText: 'Edit User Settings:' }).click();
 	await page.locator('#username').fill('Test User');
 	await page.locator('#password').fill('Test123!');
 	await page.locator('#confirm_password').fill('Test123!');
 	await page.getByRole('button').filter({ hasText: 'Save' }).click();
+	page.waitForTimeout(3000);
+
+
 });
 
 test('Registration Token', async ({ page }) => {
-	await page.goto('http://localhost:4173/login');
+	await page.goto('http://localhost:4173/login')
+	console.log('Registration Token test');
+
 	await page.locator('p').filter({ hasText: 'Sign In' }).click();
 	await page.locator('form').filter({ hasText: 'Email Address * Password * Sign In' }).locator('#email-address').fill('test@test.de');
 	await page.locator('form').filter({ hasText: 'Email Address * Password * Sign In' }).locator('#password').fill('Test123!');
@@ -82,10 +95,14 @@ test('Registration Token', async ({ page }) => {
 		await timeEkement.click();
 	}
 	await page.getByRole('button').filter({ hasText: 'Send' }).click();
+	page.waitForTimeout(3000);
+
 });
 
 test('Show or Hide User Token', async ({ page }) => {
 	await page.goto('http://localhost:4173/login');
+	console.log('Show or Hide User Token');
+
 	await page.locator('p').filter({ hasText: 'Sign In' }).click();
 	await page.locator('form').filter({ hasText: 'Email Address * Password * Sign In' }).locator('#email-address').fill('test@test.de');
 	await page.locator('form').filter({ hasText: 'Email Address * Password * Sign In' }).locator('#password').fill('Test123!');
@@ -98,10 +115,14 @@ test('Show or Hide User Token', async ({ page }) => {
 	const listElement = await page.locator('h2').filter({ hasText: 'Token List:' });
 	const isVisible = await listElement.isVisible();
 	expect(isVisible).toEqual(false);
+	page.waitForTimeout(3000);
+
 });
 
 test('Show or Hide User List', async ({ page }) => {
 	await page.goto('http://localhost:4173/login');
+	console.log('Show or Hide User List test');
+
 	await page.locator('p').filter({ hasText: 'Sign In' }).click();
 	await page.locator('form').filter({ hasText: 'Email Address * Password * Sign In' }).locator('#email-address').fill('test@test.de');
 	await page.locator('form').filter({ hasText: 'Email Address * Password * Sign In' }).locator('#password').fill('Test123!');
@@ -114,4 +135,6 @@ test('Show or Hide User List', async ({ page }) => {
 	const listElement = await page.locator('h2').filter({ hasText: 'User List:' });
 	const isVisible = await listElement.isVisible();
 	expect(isVisible).toEqual(false);
+
+	page.waitForTimeout(3000);
 });
