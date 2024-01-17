3e248cb987746531b2fc1515fcf185d428b2219e
Rar9 <RKroells@web.de>
2024-01-17 08:48:11 +0100

    fix: release

---
+++ src/components/widgets/rating/Rating.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  export let rating = 0
+  let i = 0
+</script>
+
+<div>
+  {#each Array(5) as _, i}
+    <span on:click={() => rating = i+1}>
+      {i < rating ? '★' : '☆'}
+    </span>
+  {/each}
+</div>
