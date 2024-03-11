4427f3f439f9bdd587491e5bfddeef29820f7f8f
Anis <imarahmandev@gmail.com>
2024-03-12 00:02:16 +0700

    Moved builder inside system

--- src/components/widgets/megaMenu/GuiFields.svelte
+++ src/components/widgets/megaMenu/GuiFields.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import WidgetBuilder from '@components/widgets/builder/WidgetBuilder.svelte';
+	import WidgetBuilder from '@src/components/system/builder/WidgetBuilder.svelte';
 
 	export let value: Array<Array<any>> = [];
 	//console.log(value);
