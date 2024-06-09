1c11c6525e96f2e95943b91b0370da585a299e48
Rar9 <RKroells@web.de>
2024-06-09 09:35:44 +0200

    chore: partial error fix

--- src/components/SiteName.svelte
+++ src/components/SiteName.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import { publicEnv } from '@root/config/public';
 
+	export let char: string | null = null;
+
 	const siteName = publicEnv.SITE_NAME;
 	const targetSiteName = 'SveltyCMS';
 
@@ -8,15 +10,27 @@
 	let lastPart = '';
 
 	if (siteName === targetSiteName) {
-		mainPart = siteName.slice(0, -3);
-		lastPart = siteName.slice(-3);
+		mainPart = siteName.slice(0, -3); // Get everything except the last character
+		lastPart = siteName.slice(-3); // Get only the last CMS characters
 	}
 </script>
 
-<span class="font-bold">
-	{#if siteName === targetSiteName}
-		{mainPart}<span class="text-primary-500">{lastPart}</span>
-	{:else}
-		{siteName}
-	{/if}
-</span>
+{#if char !== null}
+	<span class="font-bold">
+		{#if siteName === targetSiteName && mainPart.includes(char)}
+			{char}
+		{:else if siteName === targetSiteName && lastPart.includes(char)}
+			<span class="text-primary-500">{char}</span>
+		{:else}
+			{char}
+		{/if}
+	</span>
+{:else}
+	<span class="font-bold">
+		{#if siteName === targetSiteName}
+			{mainPart}<span class="text-primary-500">{lastPart}</span>
+		{:else}
+			{siteName}
+		{/if}
+	</span>
+{/if}
