ff68e4d8e618a5f61eae847fda157944c25151de
Rar9 <RKroells@web.de>
2024-08-05 19:17:12 +0200

    chore:fix compile.ts

--- src/routes/api/compile/+server.ts
+++ src/routes/api/compile/+server.ts
@@ -1,67 +1,32 @@
-/**
- * @file src/routes/api/compile/+server.ts
- * @description API endpoint for compiling and updating collections.
- *
- * This module handles the compilation process:
- * - Executes the compile function
- * - Updates collections
- *
- * Features:
- * - Error logging and handling
- * - Asynchronous compilation and collection update
- *
- * Usage:
- * GET /api/compile
- * No parameters required
- */
-
 import type { RequestHandler } from '@sveltejs/kit';
 import { updateCollections } from '@collections';
 import { compile } from './compile';
+
 // System Logs
 import logger from '@src/utils/logger';
 
 // Define the GET request handler
-export const GET: RequestHandler = async ({ request }) => {
-	const startTime = Date.now();
-	logger.info('Starting compilation process', { url: request.url });
-
+export const GET: RequestHandler = async () => {
 	try {
+		// Log the start of the compilation process
+		logger.debug('Starting compilation...');
+
 		// Execute the compile function
 		await compile();
-		logger.debug('Compilation completed');
 
 		// Execute the updateCollections function with the parameter 'true'
 		await updateCollections(true);
-		logger.debug('Collections updated');
 
-		const duration = Date.now() - startTime;
-		logger.info('Compilation process completed', { duration: `${duration}ms` });
+		// Log the completion of the collections update
+		logger.debug('Collections updated.');
 
 		// Return a successful response with status 200
-		return new Response(JSON.stringify({ status: 'success', duration }), {
-			status: 200,
-			headers: { 'Content-Type': 'application/json' }
-		});
+		return new Response(null, { status: 200 });
 	} catch (error) {
-		const duration = Date.now() - startTime;
-		logger.error('Error during compilation process', {
-			error: error.message,
-			stack: error.stack,
-			duration: `${duration}ms`
-		});
+		// Log any errors that occur during the GET request
+		logger.error('Error during GET /compile:', error);
 
 		// Return an error response with status 500
-		return new Response(
-			JSON.stringify({
-				status: 'error',
-				message: 'Internal server error',
-				duration
-			}),
-			{
-				status: 500,
-				headers: { 'Content-Type': 'application/json' }
-			}
-		);
+		return new Response(null, { status: 500 });
 	}
 };
--- src/routes/api/compile/compile.ts
+++ src/routes/api/compile/compile.ts
@@ -13,132 +13,107 @@
  * await compile();
  */
 
-import fs from 'fs/promises';
+import fs from 'fs';
 import crypto from 'crypto';
-import path from 'path';
-import logger from '@src/utils/logger';
 
 // Cache for transpiled modules
-const cache = new Map<string, string>();
-
-interface CompileOptions {
-	collectionsFolderJS: string;
-	collectionsFolderTS: string;
-}
+const cache = new Map();
 
 // This function compiles TypeScript files from the collections folder into JavaScript files
-export async function compile(options?: Partial<CompileOptions>): Promise<void> {
-	const {
-		// The default values for the folders are taken from the environment variables
-		collectionsFolderJS = import.meta.env.collectionsFolderJS,
-		collectionsFolderTS = import.meta.env.collectionsFolderTS
-	} = options ?? {};
-
+export async function compile({
+	// The default values for the folders are taken from the environment variables
+	collectionsFolderJS = import.meta.env.collectionsFolderJS,
+	collectionsFolderTS = import.meta.env.collectionsFolderTS
+} = {}) {
 	// console.log('Starting compilation...');
 	// console.log(`collectionsFolderJS: ${collectionsFolderJS}`);
 	// console.log(`collectionsFolderTS: ${collectionsFolderTS}`);
-	logger.info('Starting compilation process', { collectionsFolderJS, collectionsFolderTS });
 
 	// This global variable is used to store the current file name
 	globalThis.__filename = '';
 
-	try {
-		// If the collections folder for JavaScript does not exist, create it
+	// If the collections folder for JavaScript does not exist, create it
+	if (!fs.existsSync(collectionsFolderJS)) {
 		// console.log(`Creating directory: ${collectionsFolderJS}`);
-		await fs.mkdir(collectionsFolderJS, { recursive: true });
-
-		// Get the list of TypeScript files from the collections folder, excluding Auth.ts and index.ts
-		const files = (await fs.readdir(collectionsFolderTS)).filter((file) => !['index.ts'].includes(file));
-		// console.log(`Files to compile: ${files.join(', ')}`);
-		logger.debug(`Files to compile: ${files.join(', ')}`);
-
-		await Promise.all(files.map((file) => compileFile(file, collectionsFolderTS, collectionsFolderJS)));
-
-		// console.log('Compilation complete.');
-		logger.info('Compilation process completed successfully');
-	} catch (error: unknown) {
-		if (error instanceof Error) {
-			logger.error('Error during compilation process', { error: error.message });
-		} else {
-			logger.error('Unknown error during compilation process');
-		}
-		throw error;
+		fs.mkdirSync(collectionsFolderJS);
 	}
-}
 
-async function compileFile(file: string, sourcePath: string, destPath: string): Promise<void> {
-	const tsFilePath = path.join(sourcePath, file);
-	const jsFilePath = path.join(destPath, file.replace(/\.ts$/g, '.js'));
+	// Get the list of TypeScript files from the collections folder, excluding Auth.ts and index.ts
+	const files = fs.readdirSync(collectionsFolderTS).filter((file) => !['index.ts'].includes(file));
+	// console.log(`Files to compile: ${files.join(', ')}`);
 
-	try {
-		// console.log(`Compiling ${tsFilePath} to ${jsFilePath}`);
+	// Loop through each file
+	for (const file of files) {
+		try {
+			const tsFilePath = `${collectionsFolderTS.replace(/\/$/, '')}/${file}`;
+			const jsFilePath = `${collectionsFolderJS.replace(/\/$/, '')}/${file.trim().replace(/\.ts$/g, '.js')}`;
 
-		// Read the TS file as a string
-		const content = await fs.readFile(tsFilePath, 'utf-8');
-		const contentHash = crypto.createHash('md5').update(content).digest('hex');
+			// console.log(`Compiling ${tsFilePath} to ${jsFilePath}`);
 
-		if (await shouldRecompile(jsFilePath, tsFilePath, contentHash)) {
-			const code = await transpileModule(content, tsFilePath);
+			// Check if JS file exists and if TS file has been modified since last compile
+			let recompile = false;
+			if (fs.existsSync(jsFilePath)) {
+				const tsStats = fs.statSync(tsFilePath);
+				const jsStats = fs.statSync(jsFilePath);
+
+				// Create a hash of the TS file content
+				const contentHash = crypto.createHash('md5').update(fs.readFileSync(tsFilePath)).digest('hex');
+
+				// Read the existing JS file and extract the hash
+				const existingContent = fs.readFileSync(jsFilePath, { encoding: 'utf-8' });
+				const existingHash = existingContent.split('\n')[0].replace('// ', '');
+
+				// Compare hashes and modification times to determine if recompilation is necessary
+				if (contentHash === existingHash && tsStats.mtime <= jsStats.mtime) {
+					// console.log(`Skipping compilation for ${file}, no changes detected.`);
+					continue; // No need to recompile
+				}
+
+				recompile = true;
+			} else {
+				recompile = true;
+			}
+
+			// Read the TS file as a string
+			const content = fs.readFileSync(tsFilePath, { encoding: 'utf-8' });
+
+			// Check if the module is cached
+			let code = cache.get(tsFilePath);
+
+			if (!code || recompile) {
+				// Import TypeScript dynamically
+				const ts = (await import('typescript')).default;
+
+				// Transpile the TypeScript code into JavaScript code using the ESNext target
+				code = ts.transpileModule(content, {
+					compilerOptions: {
+						target: ts.ScriptTarget.ESNext
+					}
+				}).outputText;
+
+				// Replace the import statements for widgets with an empty string
+				code = code
+					.replace(/import widgets from .*\n/g, '')
+					// Replace the widgets variable with the globalThis.widgets variable
+					.replace(/widgets/g, 'globalThis.widgets')
+					// Add the .js extension to the relative import paths
+					.replace(/(\bfrom\s+["']\..*)(["'])/g, '$1.js$2');
+
+				// Cache the transpiled module
+				cache.set(tsFilePath, code);
+			}
+
 			// Prepend the content hash to the JS file
-			await fs.writeFile(jsFilePath, `// ${contentHash}\n${code}`);
+			const contentHash = crypto.createHash('md5').update(content).digest('hex');
+			code = `// ${contentHash}\n${code}`;
+
+			// Write the content to the file
+			fs.writeFileSync(jsFilePath, code);
 			// console.log(`Compiled and wrote ${jsFilePath}`);
-			logger.debug(`Compiled and wrote ${jsFilePath}`);
-		} else {
-			// console.log(`Skipping compilation for ${file}, no changes detected.`);
-			logger.debug(`Skipping compilation for ${file}, no changes detected.`);
+		} catch (error) {
+			console.error(`Error compiling ${file}: ${error}`);
+			// Handle the error appropriately
 		}
-	} catch (error: unknown) {
-		// console.error(`Error compiling ${file}: ${error}`);
-		if (error instanceof Error) {
-			logger.error(`Error compiling ${file}`, { error: error.message });
-		} else {
-			logger.error(`Unknown error compiling ${file}`);
-		}
-		throw error;
 	}
-}
-
-async function shouldRecompile(jsFilePath: string, tsFilePath: string, newContentHash: string): Promise<boolean> {
-	try {
-		const [jsStats, tsStats] = await Promise.all([fs.stat(jsFilePath).catch(() => null), fs.stat(tsFilePath)]);
-
-		if (!jsStats) return true;
-
-		// Read the existing JS file and extract the hash
-		const existingContent = await fs.readFile(jsFilePath, 'utf-8');
-		const existingHash = existingContent.split('\n')[0].replace('// ', '');
-
-		// Compare hashes and modification times to determine if recompilation is necessary
-		return newContentHash !== existingHash || tsStats.mtime > jsStats.mtime;
-	} catch {
-		return true;
-	}
-}
-
-async function transpileModule(content: string, tsFilePath: string): Promise<string> {
-	// Check if the module is cached
-	let code = cache.get(tsFilePath);
-
-	if (!code) {
-		// Import TypeScript dynamically
-		const ts = (await import('typescript')).default;
-
-		// Transpile the TypeScript code into JavaScript code using the ESNext target
-		code = ts.transpileModule(content, {
-			compilerOptions: { target: ts.ScriptTarget.ESNext }
-		}).outputText;
-
-		// Replace the import statements for widgets with an empty string
-		code = code
-			.replace(/import widgets from .*\n/g, '')
-			// Replace the widgets variable with the globalThis.widgets variable
-			.replace(/widgets/g, 'globalThis.widgets')
-			// Add the .js extension to the relative import paths
-			.replace(/(\bfrom\s+["']\..*)(["'])/g, '$1.js$2');
-
-		// Cache the transpiled module
-		cache.set(tsFilePath, code);
-	}
-
-	return code;
+	// console.log('Compilation complete.');
 }
