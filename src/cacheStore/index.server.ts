8e13bdae4a6118a726a02ac239e7bf6abdf17261
nehal7-ml <nehal.sk.99@gmail.com>
2024-11-03 00:56:59 +0530

    fix: cacheStore init on build

--- src/cacheStore/index.server.ts
+++ src/cacheStore/index.server.ts
@@ -3,9 +3,12 @@ import { privateEnv } from '@root/config/private';
 import { InMemorySessionStore } from '@src/auth/InMemoryCacheStore';
 import { RedisCacheStore } from '@src/auth/RedisCacheStore';
 
-if (!global.cacheStore) {
-	// Initialize cache to be used
-	global.cacheStore = privateEnv.USE_REDIS ? new RedisCacheStore() : new InMemorySessionStore();
-}
 
-export const cacheStore = global.cacheStore;
+
+export const getCacheStore = () => { 
+	if (!global.cacheStore) {
+		// Initialize cache to be used
+		global.cacheStore = privateEnv.USE_REDIS ? new RedisCacheStore() : new InMemorySessionStore();
+	}
+	return global.cacheStore;
+}
