a5ae44a929101cecb2d3213f40254beceb8aa29a
Anurag Pal <apal895@gmail.com>
2025-11-03 13:53:15 +0530

    fix: configure Playwright to only run playwright tests, not bun tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,7 +10,7 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-	testDir: './tests',
+	testDir: './tests/playwright',
 	testMatch: '**/*.{test,spec,spect}.ts',
 	/* Run tests in files in parallel */
 	fullyParallel: true,
