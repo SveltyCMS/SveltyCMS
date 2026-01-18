527407ed488baf945e6bd99b9e7fb78393df6721
SveltyCMS <info@sveltycms.com>
2026-01-18 12:50:26 +0100

    fix: more nx fixes

--- apps/cms/.svelte-kit/adapter-node/nodes/4.js
+++
@@ -1,8 +0,0 @@
-import * as server from '../entries/pages/_language_/_page.server.ts.js';
-
-export const index = 4;
-export { server };
-export const server_id = 'src/routes/[language]/+page.server.ts';
-export const imports = [];
-export const stylesheets = [];
-export const fonts = [];
--- apps/cms/.svelte-kit/output/server/chunks/settings.js
+++
@@ -1,8 +0,0 @@
-const baseLocale = 'en';
-const locales = ['en', 'de'];
-const inlangSettings = {
-	baseLocale,
-	locales
-};
-export { inlangSettings as i };
-//# sourceMappingURL=settings.js.map
