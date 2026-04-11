35c648d034d931fb01add418038025b5d2ff4b4a
HakamTechSol <alhakamsofts@gmail.com>
2026-04-11 15:49:47 +0500

    chore: normalize collection schemas after merge

--- src/collections/Categories.ts
+++ src/collections/Categories.ts
@@ -4,35 +4,35 @@
  * @author SveltyCMS
  */
 
-import type { Schema } from '@src/content/types';
-import { widgets } from '@src/widgets/proxy';
+import type { Schema } from "@src/content/types";
+import { widgets } from "@src/widgets/proxy";
 
 const Categories: Schema = {
-	name: 'Categories',
-	slug: 'categories',
-	icon: 'mdi:shape',
-	description: 'Blog categories',
-	fields: [
-		widgets.Input({
-			label: 'Name',
-			db_fieldName: 'name',
-			required: true,
-			translated: true,
-			width: 6
-		}),
-		widgets.Input({
-			label: 'Slug',
-			db_fieldName: 'slug',
-			required: true,
-			width: 6
-		}),
-		widgets.Input({
-			label: 'Description',
-			db_fieldName: 'description',
-			translated: true,
-			width: 12
-		})
-	]
+  name: "Categories",
+  slug: "categories",
+  icon: "mdi:shape",
+  description: "Blog categories",
+  fields: [
+    widgets.Input({
+      label: "Name",
+      db_fieldName: "name",
+      required: true,
+      translated: true,
+      width: 6,
+    }),
+    widgets.Input({
+      label: "Slug",
+      db_fieldName: "slug",
+      required: true,
+      width: 6,
+    }),
+    widgets.Input({
+      label: "Description",
+      db_fieldName: "description",
+      translated: true,
+      width: 12,
+    }),
+  ],
 };
 
 export default Categories;
