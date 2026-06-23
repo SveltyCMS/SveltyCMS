3760b3119ec60a8fd147c96339143b6184adb9d7
Tayyab Saleem <tayyab.saleem.94.69@gmail.com>
2026-06-23 19:21:03 +0500

    fix: sidebar UI + resolve API merge conflict + E2E updates + UI polishes

--- src/content/types.generated.ts
+++ src/content/types.generated.ts
@@ -7,29 +7,10 @@
 import type { CollectionEntry } from "./types";
 
 /* AUTOGEN_START: ContentTypes */
-<<<<<<< HEAD
-export type ContentTypes = "Authors" | "Categories" | "Posts" | (string & {});
+export type ContentTypes = "test_posts" | (string & {});
 
 export interface CollectionMap {
   [key: string]: CollectionEntry & Record<string, any>;
-  Authors: CollectionEntry & { name: string; bio: string; avatar: string | string[] };
-  Categories: CollectionEntry & { name: string; slug: string; description: string };
-  Posts: CollectionEntry & {
-    title: string;
-    slug: string;
-    content: string;
-    excerpt: string;
-    category: DatabaseId;
-    author: DatabaseId;
-    status: string;
-    publishDate: ISODateString;
-    seo: string;
-  };
-=======
-export type ContentTypes = string & {};
-
-export interface CollectionMap {
-  [key: string]: CollectionEntry & Record<string, any>;
->>>>>>> 9c133826f (fix: sidebar UI + resolve API merge conflict + E2E updates + UI polishes)
+  test_posts: CollectionEntry & { title: string; content: string; status: string };
 }
 /* AUTOGEN_END: ContentTypes */
